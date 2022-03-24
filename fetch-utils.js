2a9f5ec9025f74662da1229aebc56256010a1326
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-24 09:13:06 -0800

    set up basic file architecture + supabase code

---
+++ fetch-utils.js
@@ -0,0 +1,4 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6YmFwaWd1YmJzd2tobXBrbW91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc4Nzk0NTMsImV4cCI6MTk2MzQ1NTQ1M30.4fqkZViMQGidqxI8xltReNok9umY5rBiZ0lrBWSVBks';
+const SUPABASE_URL = 'https://gzbapigubbswkhmpkmou.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
