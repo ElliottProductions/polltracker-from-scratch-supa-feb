bea3ae3212152d36fa259e55f3e18cb056c43a7b
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-24 10:28:13 -0800

    got sign in form working

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 // import functions and grab DOM elements
+import { signUpUser, signInUser, logout, redirectToPolls, getUser } from './fetch-utils.js';
+//import { } from '../half-baked-soccer-keeper-feb/fetch-utils';
 
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+//const SignUpButton = document.querySelector('sign-up-button');
+console.log(signUpEmail);
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
+//const SignInButton = document.querySelector('sign-in-button');
 // let state
 
 // set event listeners 
+signUpForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    const user = await signUpUser(signUpEmail.value, signUpPassword.value);
+    console.log(`cookie?: ${user}`);
+    if (user){
+
+        redirectToPolls();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    const user = await signUpUser(signInEmail.value, signInPassword.value);
+
+    if (user){
+        redirectToPolls();
+    } else {
+        console.log(user);
+    }
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- fetch-utils.js
+++ fetch-utils.js
@@ -2,3 +2,38 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 const SUPABASE_URL = 'https://gzbapigubbswkhmpkmou.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function signUpUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
+
+export async function redirectToPolls() {
+    
+    if (await getUser()) {
+        location.replace('./poll');
+    }
+}
+
+export async function getUser() {
+    return client.auth.session();
+}
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../'); 
+}
